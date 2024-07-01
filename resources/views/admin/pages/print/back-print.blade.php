9ad61d4c79a8afea64ed41ce4f97b209809a1bfd
Aviithapa <abhishekthapa115@gmail.com>
2024-07-01 13:20:04 +0545

    changes

--- resources/views/admin/pages/print/back-print.blade.php
+++ resources/views/admin/pages/print/back-print.blade.php
@@ -249,13 +249,14 @@
                                             </div>
                                             <div class="col-lg-6">
 
-                                                 <img src="{{   $employee->getRedSignatureImage() }}"  alt="red-signature" height="20px"  /> <br />
+                                                 <img src="{{   $employee->getRedSignatureImage() }}"  alt="red-signature" height="20px" style="margin-left: 10%;" /> <br />
                                                  <span style="font-size: 8px;">{{ $employee->name_english }}</span> <br />
+                                                 <span style="font-size: 8px;">{{ $employee->designation }}</span> <br />
+                                                 <span style="font-size: 8px;">{{ $applicant->approved_date  }}</span>
                                             </div>
                                             <div class="col-lg-6">
 
-                                                <span style="font-size: 8px;">{{ $employee->designation }}</span> <br />
-                                                <span style="font-size: 8px;">{{ $applicant->approved_date  }}</span>
+
                                             </div>
                                             {{-- <div class="col-lg-3">
                                                 <span> Signature </span> <br />
